510d65829fec50342a8861938af3246395c21ba4
shakib <shakibcselimon@gmail.com>
2018-01-25 11:57:33 +0600

    All Updated

--- flask_frist_project.py
+++ flask_frist_project.py
@@ -1,8 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from random import randint
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
@@ -16,16 +14,6 @@ def members():
 
 @app.route('/hello/<string:name>')
 def hello(name):
-    quotes = [
-        "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
-        "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
-        "'To understand recursion you must first understand recursion..' -- Unknown",
-        "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
-        "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
-        "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"]
-    randomNumber = randint(0, len(quotes) - 1)
-    quote = quotes[randomNumber]
-
     return render_template(
         'test.html')
 
